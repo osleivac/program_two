a0dcd5efd77f0aaa3a93a72eab744d354b8abfd2
osleivac <os.leivac@duocuc.cl>
2025-06-21 00:08:48 -0400

    Primer commit local del proyecto program_two

---
+++ 01.program_two.py
@@ -0,0 +1,10 @@
+def leer_nota(dato):
+    while True:
+        try:
+            entrada = float(input(dato))
+            if entrada >= 1.0 and entrada <= 7.0:
+                return entrada
+            else:
+                print('Debe ingresar una nota entre 1 y 7')
+        except:
+            print('Valor no válido. Intente nuevamente.')
